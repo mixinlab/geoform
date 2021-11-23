62a360d2ce19d6a3b00acf75d9504ffdbf71edb9
Bregy Malpartida <bregy@minsky.cc>
2021-11-23 11:36:18 -0500

    fix: jsi disabled

--- examples/Basic/src/db/db.ts
+++ examples/Basic/src/db/db.ts
@@ -1,6 +1,5 @@
 import {Database} from '@nozbe/watermelondb';
 import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
-
 import migrations from './migrations';
 import Form from './models/Form';
 import Point from './models/Point';
@@ -14,7 +13,7 @@ const adapter = new SQLiteAdapter({
   // dbName: 'myapp',
   // (recommended option, should work flawlessly out of the box on iOS. On Android,
   // additional installation steps have to be taken - disable if you run into issues...)
-  jsi: true /* Platform.OS === 'ios' */,
+  jsi: false /* Platform.OS === 'ios' */,
   // (optional, but you should implement this method)
   onSetUpError: error => {
     // Database failed to load -- offer the user to reload the app or log out
